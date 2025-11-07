ead8fb4078eb5aa1797eff749743cfa1fbaae23e
cesarQuintero <131917778+Caquinterot@users.noreply.github.com>
2025-11-07 18:12:58 -0500

    Intalar dependencias

--- src/server.js
+++ src/server.js
@@ -1,25 +1,24 @@
-import 'dotenv/config'
-import app from './app.js'
-// import { sequelize } from './models/index.js'
+import 'dotenv/config';
+import app from './app.js';
+import { sequelize } from './models/index.js';
 
-const PORT = Number(process.env.PORT || 5000)
+const PORT = Number(process.env.PORT || 5000);
 
 app.listen(PORT, () => {
-	console.log(`🚀 Server escuchando en http://localhost:${PORT}`)
-})
+	console.log(`🚀 Server escuchando en http://localhost:${PORT}`);
+});
 
-// async function start() {
-// 	try {
-// 		//await sequelize.authenticate();
-// 		console.log('✅ Conexión a Postgres OK');
+async function start() {
+	try {
+		//await sequelize.authenticate();
+		console.log('✅ Conexión a Postgres OK');
 
-// 		// En desarrollo: crea/actualiza tablas automáticamente
-// 		//await sequelize.sync({ alter: true });
-// 		console.log('✅ Sincronización de modelos OK');
-
-// 	} catch (err) {
-// 		console.log('❌ Error al iniciar:', err);
-// 		process.exit(1);
-// 	}
-// }
-// start();
+		// 		// En desarrollo: crea/actualiza tablas automáticamente
+		await sequelize.sync({ alter: true });
+		console.log('✅ Sincronización de modelos OK');
+	} catch (err) {
+		console.log('❌ Error al iniciar:', err);
+		process.exit(1);
+	}
+}
+start();
